57b230f60419d1bf2d5128bda5a88f0cc05acaf9
Beadd <flyships@qq.com>
2025-11-08 19:48:54 +0800

    r

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,6 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.example.app",
+  appName: "starter-nuxt4",
   webDir: ".output/public",
   android: {
     adjustMarginsForEdgeToEdge: "force",
