0d7fa5c14c2dc5dc245082459e31ab7758d7f328
Beadd <flyships@qq.com>
2025-11-10 17:51:55 +0800

    r

---
+++ app/components/app/a.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Capacitor } from "@capacitor/core";
+
+const props = defineProps<{
+  url?: string;
+}>();
+
+async function click(e: MouseEvent) {
+  if (!props.url)
+    return;
+  if (Capacitor.isNativePlatform()) {
+    e.preventDefault();
+    useRouter().push(props.url);
+  }
+}
+</script>
+
+<template>
+  <component
+    :is="props.url ? 'a' : 'span'"
+    :href="props.url"
+    target="_blank"
+    @click="click"
+  >
+    <slot />
+  </component>
+</template>
---
+++ app/components/app/bar.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps<{
+  title?: string | null;
+  show?: boolean;
+}>();
+
+const profiles = useprofilestore();
+</script>
+
+<template>
+  <div class="sticky top-0 z-21 w-full backdrop-blur-xl">
+    <div class="h-st w-full" />
+    <div class="flex h-16 w-full border-b border-neutral">
+      <div class="flex h-full w-full items-center justify-between gap-4 px-4">
+        <div class="flex items-center gap-2">
+          <AppMenu v-if="show" class="p-0!" @click="useRouter().push('/mine')">
+            <ProfileAvatar
+              :id="profiles.item?.id || ''"
+              class="m-3 max-w-9"
+              :name="profiles.item?.name"
+              :url="profiles.item?.avatar_url"
+              :prevent="true"
+            />
+          </AppMenu>
+          <AppMenu v-else @click="useRouter().back()">
+            <i class="bx bx-chevron-left-circle text-3xl text-primary" />
+          </AppMenu>
+          <ProfileName
+            class="
+              text-2xl
+              hover:no-underline!
+            " :name="title"
+          />
+        </div>
+        <slot />
+      </div>
+    </div>
+    <slot name="sticky" />
+  </div>
+</template>
