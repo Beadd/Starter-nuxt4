dbc86c041c55f63544c19b3052485b58d223be35
Beadd <flyships@qq.com>
2025-11-10 14:32:17 +0800

    Update clipboard.client.ts

--- app/plugins/clipboard.client.ts
+++ app/plugins/clipboard.client.ts
@@ -5,7 +5,7 @@ import { Clipboard } from "@capacitor/clipboard";
 export default defineNuxtPlugin((nuxtApp) => {
   const plugins = usepluginstore();
 
-  const checkClipboard = async () => {
+  const checkclipboard = async () => {
     try {
       console.warn("clipboard check");
       const { value } = await Clipboard.read();
@@ -13,28 +13,24 @@ export default defineNuxtPlugin((nuxtApp) => {
       if (!value)
         return;
 
-      const postRegex = /^https:\/\/www\.isuishu\.com\/posts\/([a-z0-9-]+)$/i;
-      const bookRegex = /^https:\/\/www\.isuishu\.com\/books\/([a-z0-9-]+)$/i;
+      const postregex = /^https:\/\/www\.isuishu\.com\/posts\/([a-z0-9-]+)$/i;
 
-      let targetPath: string | null = null;
+      let targetpath: string | null = null;
 
-      if (postRegex.test(value)) {
-        const id = value.match(postRegex)?.[1];
-        targetPath = `/posts/${id}`;
-      } else if (bookRegex.test(value)) {
-        const id = value.match(bookRegex)?.[1];
-        targetPath = `/books/${id}`;
+      if (postregex.test(value)) {
+        const id = value.match(postregex)?.[1];
+        targetpath = `/posts/${id}`;
       }
 
-      if (targetPath && targetPath !== plugins.clipboard.ignore) {
-        plugins.clipboard.link = targetPath || "";
+      if (targetpath && targetpath !== plugins.clipboard.ignore) {
+        plugins.clipboard.link = targetpath || "";
         plugins.clipboard.bus++;
       }
     } catch (err) {
       console.error("clipboard error:", err);
     }
   };
-  nuxtApp.hook("app:mounted", checkClipboard);
+  nuxtApp.hook("app:mounted", checkclipboard);
 
-  App.addListener("resume", checkClipboard);
+  App.addListener("resume", checkclipboard);
 });
